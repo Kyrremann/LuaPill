8566c74b6dedde430ab473a1ada3f93060e99654
Kyrre Havik Eriksen <kyrre.eriksen@ciber.com>
2015-01-06 17:20:39 +0100

    Init commit with images

---
+++ main.lua
@@ -0,0 +1,108 @@
+function love.load()
+   require "setup"
+   
+   -- A tile = 133 x 99
+   TILES = {}
+   local function formatId(n)
+      if n < 10 then
+	 return "00" .. n
+      elseif n < 100 then
+	 return "0" .. n
+      end
+      return n
+   end
+
+   for i=0, 127 do
+      table.insert(TILES, gr.newImage("images/landscapeTiles_" .. formatId(i) .. ".png"))
+   end
+   index = 1
+   scrollIndex = 0
+   rotateIndex = 0
+
+   MAP = {}
+   initMap()
+end
+
+function initMap()
+   for y=1, 30 do
+      MAP[y] = {}
+      for x=1, 20 do
+	 MAP[y][x] = createTile(TILES[74], x, y)
+      end
+   end
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+--[[   
+   for y=0, 10 do
+      for x=0, 4 do
+	 drawTile(TILES[2], x, y)
+      end
+   end
+]]
+   for y, vy in ipairs(MAP) do
+      for x, tile in ipairs(vy) do
+	 drawTile(tile.tile, tile.x, tile.y)
+      end
+   end
+   gr.print("Tile: " .. index - 1, 10, 10)
+end
+
+function drawTile(tile, x, y)
+   local moX = math.floor(mo.getX() / 128)
+   local moY = math.floor(mo.getY() / 32)
+   if moX == x and moY == y then
+      tile = TILES[index]
+   elseif tile == nil then
+      tile = TILES[73]
+   end
+
+   if even(y) then
+      gr.draw(tile, x * 128, y * 32)
+   else
+      gr.draw(tile, (x * 128) + 64, y * 32)
+   end
+end
+
+function love.keypressed(key)
+   love.event.push("quit")
+end
+
+function love.mousepressed(x, y, button)
+   if button == "l" then
+      local moX = math.floor(mo.getX() / 128)
+      local moY = math.floor(mo.getY() / 32)
+      MAP[moY][moX] = createTile(TILES[index], moX, moY)
+   elseif button == "r" then
+      rotateIndex = rotateIndex + 1
+      if rotateIndex > 4 then
+	 rotateIndex = 1
+      end
+   elseif button == "wu" then
+      scrollIndex = scrollIndex + 4
+   elseif button == "wd" then
+      scrollIndex = scrollIndex - 4
+   end
+
+   index = scrollIndex + rotateIndex
+   if index < 1 then
+      index = #TILES
+   elseif index > #TILES then
+      index = 1
+   end
+end
+
+function createTile(tile, x, y)
+   return {
+      tile = tile,
+      x = x,
+      y = y
+   }
+end
+
+function even(n)
+   return n % 2 == 0
+end
---
+++ setup.lua
@@ -0,0 +1,11 @@
+gr = love.graphics
+ke = love.keyboard
+mo = love.mouse
+
+ti = love.timer
+im = love.image
+ev = love.event
+so = love.sound
+au = love.audio
+
+require "math"
