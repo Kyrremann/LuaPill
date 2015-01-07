083487dab580a98ac3e794fc9b0339fd57575c2b
Kyrre Havik Eriksen <kyrremann@gmail.com>
2015-01-07 23:06:34 +0100

    Doing some testing to convert mouse cords to map using algebra

--- main.lua
+++ main.lua
@@ -8,9 +8,11 @@ function love.load()
    SCROLLMODE = false
    TILESCALE = 1
    CAMERA = {
-      x = 0,
+      x = gr.getWidth() / 2,
       y = 0
    }
+   TILE_WIDTH_HALF = 64  -- 132 / 2
+   TILE_HEIGHT_HALF = 32 -- 66 / 2
 
    index = 1
    scrollIndex = 0
@@ -38,7 +40,7 @@ end
 function initMap()
    for y=1, 30 do
       MAP[y] = {}
-      for x=1, 20 do
+      for x=1, 30 do
 	 MAP[y][x] = createTile(TILES[74], x, y)
       end
    end
@@ -48,21 +50,31 @@ function love.update(dt)
 end
 
 function love.draw()
+   local screen = {}
+   local map = screenToMap({ x = mo.getX(), y = mo.getY() })
+   gr.print(map.x .. "," .. map.y, 10, 45)
    gr.push()
    gr.translate(CAMERA.x, CAMERA.y)
    for y, vy in ipairs(MAP) do
-      for x, tile in ipairs(vy) do
-	 drawTile(tile.tile, tile.x, tile.y)
+      for x, shape in ipairs(vy) do
+	 local tile = shape.tile
+	 screen = mapToScreen({ x = x, y = y })
+	 -- gr.draw(TILES[74], screen.x, screen.y)
+	 if map.x == x and map.y == y then
+	    tile = TILES[index]
+	 elseif not tile then
+	    tile = TILES[74]
+	 end
+
+	 gr.draw(tile, -- drawable
+	    screen.x * TILESCALE, screen.y * TILESCALE, -- cords
+	    0, -- rotation
+	    TILESCALE, TILESCALE -- scale
+	 )
+	 -- drawTile(tile.tile, tile.x, tile.y)
       end
    end
    gr.pop()
-
-   -- debug data
-   local moX = math.floor(mo.getX() / 128)
-   local moY = math.floor(mo.getY() / 32)
-   gr.print("(" .. mo.getX() .. "," .. mo.getY() .. ")", 10, 45)
-   gr.print("(" .. mo.getX() / 128 .. "," .. mo.getY() / 32 .. ")", 10, 65)
-   gr.print("(" .. moX .. "," .. moY .. ")", 10, 85)
 
    gr.print("Tile: " .. index, 10, 10)
    gr.print("Scale: " .. TILESCALE, 10, 25)
@@ -157,6 +169,20 @@ function createTile(tile, x, y)
       x = x,
       y = y
    }
+end
+
+function mapToScreen(map)
+   local screen = {}
+   screen.x = (map.x - map.y) * TILE_WIDTH_HALF;
+   screen.y = (map.x + map.y) * TILE_HEIGHT_HALF
+   return screen
+end
+
+function screenToMap(screen)
+   local map = {}
+   map.x = math.floor((screen.x / TILE_WIDTH_HALF + screen.y / TILE_HEIGHT_HALF) / 2)
+   map.y = math.floor((screen.y / TILE_HEIGHT_HALF -(screen.x / TILE_WIDTH_HALF)) / 2)
+   return map
 end
 
 function odd(n)
