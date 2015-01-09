78331afb6a82525ad31c35ae2e496cdb60e13a1f
Kyrre Havik Eriksen <Kyrremann@gmail.com>
2015-01-09 01:46:32 +0100

    Added a simple sidebar to show the tiles that you are scrolling through

--- main.lua
+++ main.lua
@@ -11,8 +11,42 @@ end
 function love.draw()
    map:draw()
 
+   drawSidebar()
+
+   gr.setColor(255, 255, 255)
    gr.print("Tile: " .. map:getTileIndex(), 10, 10)
    gr.print("Scale: " .. map:getScale(), 10, 25)
+end
+
+function drawSidebar()
+   local index = map:getTileIndex()
+
+   gr.setColor(0, 0, 0)
+   gr.rectangle("fill", 0, 0, 200, gr.getHeight())
+
+   gr.setColor(255, 255, 255)
+   local sh = gr.getHeight() / 2
+   local set = {
+      index - 2,
+      index - 1,
+      index,
+      index + 1,
+      index + 2
+   }
+
+   for v, k in ipairs(set) do
+      if k == 0 then set[v] = 128
+      elseif k == -1 then set[v] = 127
+      elseif k == 129 then set[v] = 1
+      elseif k == 130 then set[v] = 2
+      end
+   end
+   
+   gr.draw(map:getTile(set[1]), 64, sh - 128 * 2, 0, .5, .5)
+   gr.draw(map:getTile(set[2]), 46, sh - 128, 0, .75, .75)
+   gr.draw(map:getTile(set[3]), 36, sh)
+   gr.draw(map:getTile(set[4]), 46, sh + 128, 0, .75, .75)
+   gr.draw(map:getTile(set[5]), 64, sh + 128 * 2, 0, .5, .5)
 end
 
 function love.keypressed(key)
