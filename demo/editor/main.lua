25a28ade1e787050a18126f712b5374a77be0470
Kyrre Havik <kyrremann@gmail.com>
2025-01-02 15:45:26 +0100

    upkeep: uppercase global logical name

--- demo/editor/main.lua
+++ demo/editor/main.lua
@@ -12,7 +12,7 @@ function love.load()
    SHOW_HELP = false
    SCALEMODE = false
 
-   map = require "luapill"
+   PILL = require "luapill"
    local config = {
 	  tilewidth = 226, -- 256, the object is smaller than the tile
 	  tileheight = 216/2, -- 352, the object is smaller than the tile
@@ -20,7 +20,7 @@ function love.load()
 	  defaultTile = 114,
 	  tileIndex = 114,
    }
-   map:setup(config)
+   PILL:setup(config)
 
    -- move mouse to center
    love.mouse.setPosition(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2)
@@ -31,14 +31,14 @@ end
 
 function love.draw()
    love.graphics.setColor(255, 255, 255)
-   map:draw()
+   PILL:draw()
 
    drawSidebar()
 
    love.graphics.setColor(255, 255, 255)
-   love.graphics.print("Tile: " .. map:getTileIndex(), 10, 10)
-   love.graphics.print("Level: " .. map:getLevel(), 10, 25)
-   love.graphics.print("Scale: " .. map:getScale(), 10, 40)
+   love.graphics.print("Tile: " .. PILL:getTileIndex(), 10, 10)
+   love.graphics.print("Level: " .. PILL:getLevel(), 10, 25)
+   love.graphics.print("Scale: " .. PILL:getScale(), 10, 40)
    love.graphics.print("Press 'h' for keys", love.graphics.getWidth() * .8, 10)
 
 
@@ -49,7 +49,7 @@ function love.draw()
 end
 
 function drawSidebar()
-   local index = map:getTileIndex()
+   local index = PILL:getTileIndex()
 
    love.graphics.setColor(0, 0, 0)
    love.graphics.rectangle("fill", 0, 0, 200, love.graphics.getHeight())
@@ -64,7 +64,7 @@ function drawSidebar()
 	  index + 2
    }
 
-   local size = map:getTileCount()
+   local size = PILL:getTileCount()
    for v, k in ipairs(set) do
 	  if k == 0 then set[v] = size
 	  elseif k == -1 then set[v] = size - 1
@@ -73,13 +73,13 @@ function drawSidebar()
 	  end
    end
 
-   local height = map:getTile(1):getHeight() / 4
+   local height = PILL:getTile(1):getHeight() / 4
 
-   love.graphics.draw(map:getTile(set[1]), 64, sh - height * 2.75, 0, .25, .25)
-   love.graphics.draw(map:getTile(set[2]), 46, sh - height * 2, 0, .38, .38)
-   love.graphics.draw(map:getTile(set[3]), 36, sh - height, 0, .5, .5)
-   love.graphics.draw(map:getTile(set[4]), 46, sh + height * .75, 0, .38, .38)
-   love.graphics.draw(map:getTile(set[5]), 64, sh + height * 2.25, 0, .25, .25)
+   love.graphics.draw(PILL:getTile(set[1]), 64, sh - height * 2.75, 0, .25, .25)
+   love.graphics.draw(PILL:getTile(set[2]), 46, sh - height * 2, 0, .38, .38)
+   love.graphics.draw(PILL:getTile(set[3]), 36, sh - height, 0, .5, .5)
+   love.graphics.draw(PILL:getTile(set[4]), 46, sh + height * .75, 0, .38, .38)
+   love.graphics.draw(PILL:getTile(set[5]), 64, sh + height * 2.25, 0, .25, .25)
 end
 
 function drawHelpScreen()
@@ -107,30 +107,30 @@ function love.keypressed(key, scancode, isrepeat)
 	  SHOW_HELP = not SHOW_HELP
    elseif key == '+' then
 	  if SCALEMODE then
-		 map:zoomMap(map:getScale() + .2)
+		 PILL:zoomMap(PILL:getScale() + .2)
 	  else
-		 map:shiftTile(1)
+		 PILL:shiftTile(1)
 	  end
    elseif key == "-" then
 	  if SCALEMODE then
-		 map:zoomMap(map:getScale() - .2)
+		 PILL:zoomMap(PILL:getScale() - .2)
 	  else
-		 map:shiftTile(-1)
+		 PILL:shiftTile(-1)
 	  end
    elseif key == "lshift" then
 	  SCALEMODE = true
    elseif key == "lalt" then
 	  LEVELMODE = true
    elseif key == "w" then
-	  map:moveCamera(0, 10)
+	  PILL:moveCamera(0, 10)
    elseif key == "s" then
-	  map:moveCamera(0, -10)
+	  PILL:moveCamera(0, -10)
    elseif key == "a" then
-	  map:moveCamera(10, 0)
+	  PILL:moveCamera(10, 0)
    elseif key == "d" then
-	  map:moveCamera(-10, 0)
+	  PILL:moveCamera(-10, 0)
    elseif key == "f5" then
-	  local success, message = map:saveMap()
+	  local success, message = PILL:saveMap()
 	  if success then
 		 print('file created')
 	  else
@@ -139,7 +139,7 @@ function love.keypressed(key, scancode, isrepeat)
    elseif key == "f6" then
 	  -- TODO: Add a file dialog
 	  local path = "default_181.47793070833.luapill"
-	  map:loadMap(path)
+	  PILL:loadMap(path)
    end
 end
 
@@ -153,13 +153,13 @@ end
 
 function love.mousepressed(x, y, button, istouch)
    if button == 1 then
-	  map:placeTile()
+	  PILL:placeTile()
    end
 end
 
 function love.mousereleased(x, y, button, istouch)
    if button == 2 and not MOVED_MAP then
-	  map:deleteTile()
+	  PILL:deleteTile()
    end
 
    MOVED_MAP = false
@@ -167,29 +167,29 @@ end
 
 function love.mousemoved(x, y, dx, dy, istouch)
    if love.mouse.isDown(2) then
-	  map:moveCamera(dx, dy)
+	  PILL:moveCamera(dx, dy)
 	  MOVED_MAP = true
    elseif love.mouse.isDown(1) then
-	  map:placeTile()
+	  PILL:placeTile()
    end
 end
 
 function love.wheelmoved(x, y)
    if y > 0 then
 	  if LEVELMODE then
-		 map:shiftLevel(1)
+		 PILL:shiftLevel(1)
 	  else
-		 map:shiftTile(1)
+		 PILL:shiftTile(1)
 	  end
    elseif y < 0 then
 	  if LEVELMODE then
-		 map:shiftLevel(-1)
+		 PILL:shiftLevel(-1)
 	  else
-		 map:shiftTile(-1)
+		 PILL:shiftTile(-1)
 	  end
    elseif x > 0 then
-	  map:zoomMap(map:getScale() + .2)
+	  PILL:zoomMap(PILL:getScale() + .2)
    elseif x < 0 then
-	  map:zoomMap(map:getScale() - .2)
+	  PILL:zoomMap(PILL:getScale() - .2)
    end
 end
